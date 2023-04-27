a718993d9eef842962bb366fa7a650816f495b73
thanh trinh <thanhtrinhviet09@gmail.com>
2023-04-27 15:10:29 +0700

    update

---
+++ C.backend/order-service/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { myDataSource } from "./app-data-source";
+// establish database connection
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
